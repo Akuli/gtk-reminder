c4c33192d0598b906a4f9ce2dc23a96f1f328cdd
Akuli <akuviljanen17@gmail.com>
2016-10-23 19:14:36 +0300

    fix the header file

--- reminder.h
+++ reminder.h
@@ -13,6 +13,7 @@ void reminder_convert_time(
 	unsigned int seconds, int * minutetarget,
 	int * secondtarget);
 void reminder_format_time(int minutes, int seconds, char * target);
+void reminder_make_gtk_button_big(GtkWidget * button);
 Reminder * reminder_new(void);
 gboolean reminder_on_click(GtkWidget * button, gpointer data);
 void reminder_start(Reminder * reminder);
